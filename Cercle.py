9197e67c85028238b678617d2414746ccc5abea4
Leo Garcin E sfa l2-sts-stic <garcinl@localhost>
2015-03-16 17:06:19 +0100

    bla

--- Cercle.py
+++ Cercle.py
@@ -2,8 +2,8 @@ from math import sqrt
 def cercle(x, y, rayon, couleur, taille):
 	t=[[0 for x in range(taille)] for x in range(taille)]
 
-	for i in range (x, taille, 1):
-		for j in  range (y, taille, 1):
+	for i in range (0, taille, 1):
+		for j in  range (0, taille, 1):
 			if sqrt((x-i)**2 + (y-j)**2)<=rayon:
 				t[i][j]=couleur
 		for i in range(taille):
