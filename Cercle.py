4f5661c91aa5ea5befe526d7b62ad9acb19f3a2c
Leo Garcin E sfa l2-sts-stic <garcinl@localhost>
2015-03-16 16:30:01 +0100

    Mise a jour cercle et makefile

--- Cercle.py
+++ Cercle.py
@@ -1,6 +1,7 @@
-def cercle(x, y, rayon=5, couleur, taille):
-	t=[0 for x in range(taille)] for x in range(taille)]
+from math import sqrt
+def cercle(x, y, rayon, couleur, taille):
+	t=[[0 for x in range(taille)] for x in range(taille)]
 	for i in range (x, taille, 1):
 		for j in  range (y, taille, 1):
-			if sqr((x-i)**2 + (y-j)**2)<=rayon:
+			if sqrt((x-i)**2 + (y-j)**2)<=rayon:
 				t[i][j]=couleur
--- makefile
+++ makefile
@@ -1,1 +1,6 @@
-all: 
+all : prog prog2
+prog :
+prog2 : cercle.py triangle.py rectangle.py
+	python -m py compile cercle.py
+	python -m py compile triangle.py
+	python -m py compile rectangle.py
