c73c5aba5c5a15b9e5689a3d3672580aff40d4df
estoraph <estoraph@gmail.com>
2015-03-16 16:55:29 +0100

    rectangle finit

--- rectangle.py
+++ rectangle.py
@@ -1,10 +1,21 @@
-def rectangle(posx,posy,longueur,largeur,couleur,taille):
-	t=[[0 for x in range(taille)] for x in range (taille)]
-	for i in range(posx,longueur+posx,1):
-		for j in range(posy,largeur+posy,1):
-			t[i][j]=couleur
+def rectangle(posx,posy,longueur,largeur,couleur,couleur2,taille):
+    t=[[couleur2 for x in range(taille)] for x in range (taille)]
+    taillev=longueur+posx;
+    tailleh=largeur+posy;
+    if taillev > taille or tailleh > taille:
+        print("Votre dessin a depassé la matrice");
+    else:
+        
+        for i in range(posx,longueur+posx,1):
+            for j in range(posy,largeur+posy,1):
+                t[i][j]=couleur;
+        for i in range(taille):
+            print("");
+    
+            for j in range(taille):
+                print(str(t[i][j]),end=" ");
 
-	print(t)
+    
 
 
 
