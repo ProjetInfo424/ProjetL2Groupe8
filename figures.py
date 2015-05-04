72b17097f0c1c00511d26096884e049dc02ced82
estoraph <estoraph@gmail.com>
2015-05-04 12:54:15 +0200

    ajout des couleurs et du argv

--- figures.py
+++ figures.py
@@ -1,12 +1,36 @@
 from math import sqrt
+from sys import argv
 #On a besoin de la bibliotheque python math pour utiliser la fonction racine carrée
 
-def matrice(largeuri,hauteuri,couleuri1,couleuri2,couleuri3):
-    t=[couleuri1,couleuri2,couleuri3];
-    tmat=[[t for x in range(hauteuri)] for x in range (largeuri)];
+def couleur_ppm (couleur) :
+    if couleur == "blanc" :
+        return [255,255,255]
+    if couleur == "noir" :
+        return [0,0,0]
+    if couleur == "bleu" :
+        return [0,0,255]
+    if couleur == "jaune" :
+        return [255,255,0]
+    if couleur == "rouge" :
+        return [255,0,0]
+    if couleur == " violet":
+        return [102, 0, 153]
+    if couleur == "vert" :
+        return [0 ,255, 0]
+    if couleur == "rose" :
+        return [253, 108, 158]
+    if couleur == "orange":
+        return [237, 127, 16]
+
+def matrice(largeuri,hauteuri,couleuri):
+    couleur_im = couleur_ppm(couleuri);
+    tmat=[[couleur_im for x in range(hauteuri)] for x in range (largeuri)];
     return tmat
+
+
+
     
-def rectangle(posx,posy,largeurf,hauteurf,couleurf1,couleurf2,couleurf3,largeuri,hauteuri,couleuri1,couleuri2,couleuri3):
+def rectangle(posx,posy,largeurf,hauteurf,couleurf,largeuri,hauteuri,couleuri):
 
     ## Cette procedure permet de créer un rectangle. Ses paramètres sont:
     ## posx --> position de la figure par rapport a l'abscisse 
@@ -17,11 +41,13 @@ def rectangle(posx,posy,largeurf,hauteurf,couleurf1,couleurf2,couleurf3,largeuri
     ## hauteuri --> hauteur de l'image (verticale)
     ## couleurf --> couleur de la figure (un entier)
     ## couleuri --> couleur de l'image (un entier)
+
+    couleur_fi = couleur_ppm(couleurf)
+
+
     print("P3"+ "\n" + str(largeuri) + " " + str(hauteuri) +"\n" + "255");
 
-
-    tab_figure=[couleurf1,couleurf2,couleurf3];
-    t=matrice(largeuri,hauteuri,couleuri1,couleuri2,couleuri3);
+    t=matrice(largeuri,hauteuri,couleuri);
     tailleh=largeurf+posx;
     taillev=hauteurf+posy;
     
@@ -31,7 +57,7 @@ def rectangle(posx,posy,largeurf,hauteurf,couleurf1,couleurf2,couleurf3,largeuri
         
         for i in range(posy,taillev,1):
             for j in range(posx,tailleh,1): 
-                t[j][i]=tab_figure;
+                t[j][i]=couleur_fi;
     for k in range(hauteuri):
         
         for l in range(largeuri):
@@ -46,7 +72,7 @@ def rectangle(posx,posy,largeurf,hauteurf,couleurf1,couleurf2,couleurf3,largeuri
 
 
 
-def cercle(x, y, rayon, hauteuri,largeuri, couleurf1, couleurf2, couleurf3, couleuri1, couleuri2, couleuri3 ):
+def cercle(x, y, rayon, hauteuri,largeuri,couleuri ,couleurf) :
 #Fonction qui dessine un cercle dans une image. Elle a pour arguments :
 #x : abscisse du centre du cercle
 #y : ordonnée du centre du cercle        
@@ -57,14 +83,14 @@ def cercle(x, y, rayon, hauteuri,largeuri, couleurf1, couleurf2, couleurf3, coul
 #largeuri : la largeur de l'image
     print("P3"+ "\n" + str(largeuri) + " "+ str(hauteuri)+"\n" + "255");
 
-    t= matrice(largeuri,hauteuri,couleuri1, couleuri2, couleuri3);
-    tab_figure=[couleurf1,couleurf2,couleurf3];
+    t= matrice(largeuri,hauteuri,couleuri);
+    couleur_fi = couleur_ppm(couleurf)
     
     for i in range (hauteuri):
         for j in  range (largeuri):
             if sqrt((x-j)**2 + (y-i)**2)<=rayon:
             #Pour chaque pixel on verifie si il est a une distance inferieure ou egale au rayon du centre.
-                t[j][i]=tab_figure;
+                t[j][i]=couleur_fi;
     for k in range(hauteuri):
         
         for l in range(largeuri):
@@ -76,9 +102,9 @@ def cercle(x, y, rayon, hauteuri,largeuri, couleurf1, couleurf2, couleurf3, coul
         print("");
 
 
-def trace_segment(t,x0,y0,x1,y1,couleurf1, couleurf2, couleurf3):
+def trace_segment(t,x0,y0,x1,y1,couleurf):
     
-    tab_figure=[couleurf1,couleurf2,couleurf3];
+    couleur_fi = couleur_ppm(couleurf);
     tmp=0
     tmp2=0
     if x1 < x0 :
@@ -98,44 +124,41 @@ def trace_segment(t,x0,y0,x1,y1,couleurf1, couleurf2, couleurf3):
     if y1-y0< 0:
         sign= -1
     for x in range(x0,x1,1):
-        t[x][y]=tab_figure
+        t[x][y]=couleur_fi
         error=error+deltaerror
         while error >= 0.5:
-            t[x][y]= tab_figure
+            t[x][y]= couleur_fi
             y=y+sign
             error=error-1
             
     
-def triangle_rempli(t,x0,y0,x1,y1,x2,y2,largeuri,hauteuri,couleurf1, couleurf2, couleurf3):
+def triangle_rempli(t,x0,y0,x1,y1,x2,y2,largeuri,hauteuri,couleurf):
     compteur=0
     liste =[]
-    tab_figure=[couleurf1,couleurf2,couleurf3];
+    couleur_fi = couleur_ppm(couleurf)
     
     for i in range (hauteuri):
         for j in range (largeuri):
-            if t[i][j] == tab_figure:
+            if t[i][j] == couleur_fi:
                 liste = liste + [j]
                 compteur = compteur + 1
         if compteur > 1:
             for x in range(liste[0],liste[len(liste)-1],1):
-                t[i][x]= tab_figure
+                t[i][x]= couleur_fi
             compteur =0  
     
     
 
-def triangle(x0,y0,x1,y1,x2,y2,largeuri,hauteuri,couleurf1, couleurf2, couleurf3, couleuri1, couleuri2, couleuri3 ):
+def triangle(x0,y0,x1,y1,x2,y2,largeuri,hauteuri, couleuri ,couleurf):
 
     print("P3"+ "\n" + str(largeuri) + " "+ str(hauteuri)+"\n" + "255");  
 
-    t=matrice(largeuri,hauteuri,couleuri1,couleuri2,couleuri3);
-    print( "P0 = ({},{})".format( x0, y0 ) )
-    print( "P1 = ({},{})".format( x1, y1 ) )
-    print( "P2 = ({},{})".format( x2, y2 ) )
+    t=matrice(largeuri,hauteuri,couleuri);
     
-    trace_segment(t,x0,y0,x1,y1,couleurf1, couleurf2, couleurf3);
-    trace_segment(t,x1,y1,x2,y2,couleurf1, couleurf2, couleurf3);
-    trace_segment(t,x2,y2,x0,y0,couleurf1, couleurf2, couleurf3);
-    triangle_rempli(t,x0,y0,x1,y1,x2,y2,largeuri,hauteuri,couleurf1, couleurf2, couleurf3);
+    trace_segment(t,x0,y0,x1,y1,couleurf);
+    trace_segment(t,x1,y1,x2,y2,couleurf);
+    trace_segment(t,x2,y2,x0,y0,couleurf);
+    triangle_rempli(t,x0,y0,x1,y1,x2,y2,largeuri,hauteuri,couleurf);
     
 
     for k in range(hauteuri):
@@ -148,20 +171,54 @@ def triangle(x0,y0,x1,y1,x2,y2,largeuri,hauteuri,couleurf1, couleurf2, couleurf3
             print(enleve,end= " ");
         print("");
 
-##if __name__ == '__main__':
-##    print(argv)
-##def Lire_arg(argv):
-##    if argv[1]=="-cercle":
-##        cercle(int(argv[2]), int(argv[3]), int(argv[4]), int(argv[5]), int(argv[6]), int(argv[7]), int(argv[8]));
-##    elif argv[1]=="-rectangle":
-##        rectangle(int(argv[2]),int(argv[3]),int(argv[4]),int(argv[5]),int(argv[6]),int(argv[7]),int(argv[8]),int(argv[9]));
-##    elif argv[1]=="-triangle":
-##        triangle(int(argv[2]),int(argv[3]),int(argv[4]),int(argv[5]),int(argv[6]),int(argv[7]),int(argv[8]),int(argv[9]),int(argv[10]),int(argv[11]));
-##    else :
-##        print("Vos arguments ne sont pas valides, pour plus d'informations veuillez consulter le README.txt");
-##Lire_arg(argv);
+def menu(forme):
 
-rectangle(2,2,5,4,0,1,2,10,10,4,5,6)
+          
+    if forme == "rectangle" :
+        
+        posx= int(argv[2])
+        posy = int(argv[3])
+        largeurf =  int(argv[4])
+        hauteurf = int( argv[5] )
+        couleurf =str( argv[6] )
+        largeuri=int(argv[7])
+        hauteuri=  int( argv[8] )
+        couleuri=str(argv[9])
+        rectangle(posx,posy,largeurf,hauteurf,couleurf,largeuri,hauteuri,couleuri);
+       
+    if forme == "cercle" :
+
+        x = int(argv[2])
+        y = int(argv[3])
+        rayon =  int(argv[4])
+        hauteuri = int( argv[5] )
+        largeuri =int( argv[6] )
+        couleuri=str(argv[7])
+        couleurf=  str( argv[8] )
+        
+        cercle(x, y, rayon, hauteuri,largeuri,couleuri ,couleurf)        
+        
+  
+        
+    if forme == "triangle" :
+        
+        x0= int(argv[2])
+        y0 = int(argv[3])
+        x1 =  int(argv[4])
+        y1 = int( argv[5] )
+        x2 =int( argv[6] )
+        y2=int(argv[7])
+        largeuri=  int( argv[8] )
+        hauteuri=int(argv[9])
+        couleuri=str(argv[10])
+        couleurf=str(argv[11])
+
+        
+        triangle(x0,y0,x1,y1,x2,y2,largeuri,hauteuri, couleuri ,couleurf)
+
+if __name__ == '__main__' :
+
+   menu(argv[1]);
     
 
   
